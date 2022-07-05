9cc836dbdc61848560345f5410aa648bf396f52a
Paulo Lucena <paulo.henriquegvl@gmail.com>
2022-07-05 15:42:03 -0300

    implementado delete no Seller

--- SalesWebAPI/Interfaces/ISellerService.cs
+++ SalesWebAPI/Interfaces/ISellerService.cs
@@ -7,5 +7,9 @@ namespace SalesWebAPI.Interfaces
         List<Seller> FindAll();
         
         void Insert(Seller seller);
+
+        Seller FindById(int id);
+
+        void Remove(int id);
     }
 }
