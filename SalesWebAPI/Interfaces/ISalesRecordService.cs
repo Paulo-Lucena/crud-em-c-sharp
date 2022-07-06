39031497153d813db6b87e9db24442d95aa44328
Paulo Lucena <paulo.henriquegvl@gmail.com>
2022-07-06 01:26:18 -0300

    finished

--- SalesWebAPI/Interfaces/ISalesRecordService.cs
+++ SalesWebAPI/Interfaces/ISalesRecordService.cs
@@ -4,6 +4,7 @@ namespace SalesWebAPI.Interfaces
 {
     public interface ISalesRecordService
     {
-       Task <List<SalesRecord>> FindByDateAsync(DateTime? minDate, DateTime? maxDate);
+        Task<List<SalesRecord>> FindByDateAsync(DateTime? minDate, DateTime? maxDate);
+        Task<List<IGrouping<Department, SalesRecord>>> FindByDateGroupingAsync(DateTime? minDate, DateTime? maxDate);
     }
 }
